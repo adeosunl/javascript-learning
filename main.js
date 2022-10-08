c2b9462690ba5e19a57d84ea0b2928ed908abf74
olumide <adeosunolumidefikayomi@gmail.com>
2022-10-08 13:34:42 +0100

    initialise git

---
+++ main.js
@@ -0,0 +1,19 @@
+const cars = ["toyota" , "volvo" , "benz"]
+console.log(cars)
+
+// const foods = ["ofadaRice" , "pepperSoup" , "friedRice" , "jollofRice"]
+
+// console.log(foods)
+
+let foodList = ["ofadaRice" , "pepperSoup" , "friedRice" , "jollofRice"]
+
+console.log(foodList)
+
+let w = 500;
+let x = 500;
+let y = 195;
+let z = w + x + y;
+
+document.getElementById("demo").innerHTML = z;
+
+console.log(z)
